b1f93b7adda53a05190fc855177ff1dae778e320
Alex Goulart <goowlart@gmail.com>
2018-11-08 12:01:43 +0100

    Upgrade merge to version 1.2.1 or later and reorganization of archives

--- airbnb-server/start/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Route = use('Route')
-
-Route.post('/users', 'UserController.create')
-Route.post('/sessions', 'SessionController.create')
-Route.resource('properties', 'PropertyController')
-  .apiOnly()
-  .middleware('auth')
